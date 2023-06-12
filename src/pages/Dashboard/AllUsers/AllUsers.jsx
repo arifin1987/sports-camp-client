336c351738e21d755137110f14b7a59c7fe0bd93
Shohidul Arifin <shohidularifin@gmail.com>
2023-06-11 23:01:36 -0700

    admin and instructot role set to all users page

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import { FaTrash, FaUser } from "react-icons/fa";
+import { FaSuperpowers, FaTrash, FaUser, FaUserGraduate, FaUserShield } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 
 const AllUsers = () => {
@@ -7,8 +8,45 @@ const AllUsers = () => {
         const res = await fetch('http://localhost:5000/users')
         return res.json();
     })
-    const handleMakeAdmin= id =>{
-        
+    const handleMakeAdmin = user =>{
+        fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: 'PATCH'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.modifiedCount){
+                refetch();
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${user.name} is an Admin `,
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+        })
+    }
+
+    const handleMakeInstructor = user=>{
+        fetch(`http://localhost:5000/users/instructor/${user._id}`,{
+            method:'PATCH'
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            if(data.modifiedCount){
+                refetch();
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${user.name} is an Instructor `,
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+
+        })
+
     }
     return (
         <div>
@@ -18,13 +56,14 @@ const AllUsers = () => {
            
            
             <div className="overflow-x-auto w-full">
-                <table className="table w-full">
+                <table className="table w-full bg-sky-300">
                     
                     <thead>
                         <tr>
                             <th>#</th>
                             <th>Name</th>
                             <th>Email</th>
+                            <th>Role</th>
                             <th>Role</th>
                             
                             <th>Action</th>
@@ -49,7 +88,17 @@ const AllUsers = () => {
                                 <td>
                                     {
                                     user.role ==='admin'? 'admin':
-                                    <button onClick={()=>handleMakeAdmin(user._id)}><FaUser></FaUser></button>
+                                    <button onClick={()=>handleMakeAdmin(user)}><FaUserShield></FaUserShield></button>
+                                    
+                                    }
+                                    
+                                    
+
+                                </td>
+                                <td>
+                                    {
+                                    user.role ==='instructor'? 'instructor':
+                                    <button onClick={()=>handleMakeInstructor(user)}><FaUserGraduate></FaUserGraduate></button>
                                     
                                     }
                                     
