ffe8a972de0a4d7149859109b26e214b557331a4
Shohidul Arifin <shohidularifin@gmail.com>
2023-06-12 05:31:57 -0700

    json web token verified using asiosSecure

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,15 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "./useAuth"
+import useAxiosSecure from "./useAxiosSecure";
 
 
 const useCart=()=>{
     const {user} = useAuth();
+    
+    const [axiosSecure]= useAxiosSecure();
 
     const{refetch, data:cart=[]}= useQuery({
         queryKey:['carts', user?.email],
         queryFn:async ()=>{
-            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
-            return res.json();
+            const res = await axiosSecure(`/carts?email=${user?.email}`)
+            return res.data;
         }
     })
     return[cart,refetch];
